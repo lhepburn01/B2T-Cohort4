0ad10868a682be27d4221139e3db1a6b1ca39e34
lhepburn01 <lauren.hepburn@gmail.com>
2018-10-16 15:44:39 -0400

    displayname

---
+++ namedisplay/script.js
@@ -0,0 +1,11 @@
+
+function getFormValue(){
+	let fname = document.getElementById('fname').value;
+	let lname = document.getElementById('lname').value;
+
+	fullName = fname + ' ' + lname;
+
+	document.getElementById('answerDisplay').innerHTML = 'Hello ' + fullName;
+}
+
+
