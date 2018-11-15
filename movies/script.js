ff957f79a215fc02e25196bf7e8fdfdc81af2318
Lauren H <43757890+lhepburn01@users.noreply.github.com>
2018-11-15 15:18:13 -0500

    Add files via upload
    revised to add images to the catalogue

--- movies/script.js
+++ movies/script.js
@@ -1,20 +1,25 @@
 let movies = [
-	{title:'Superman', director:'MJ', year:1982, status: 'Yes'},
-	{title:'Batman', director:'Snoop', year:1994, status: 'No'},
-	{title:'Wonder Woman', director:'PK', year:2017, status: 'No'}
+	{title:'Black Panther', director:'Ryan Coogler', year:2018, status: 'Yes', cover:'images/blackPanther.jpg'},
+	{title:'Bohemian Rhapsody', director:'Bryan Singer', year:2018, status: 'No', cover:'images/BohRhap.jpg'},
+	{title:'Jaws', director:'Steven Spielberg', year: 1975, status: 'Yes', cover:'images/jaws.JPG'},
+	{title:'Pulp Fiction', director:'Quetin Tarantino', year: 1994, status: 'No', cover:'images/pulpFiction.jpg'},
+	{title:'Moonlight', director:'Barry Jenkins', year: 2016, status: 'No', cover:'images/moonlight.jpg'}
 ];
+
 
 function addMovie(){
 	let movieTitle = document.getElementById('title').value;
 	let movieDirector = document.getElementById('director').value;
 	let movieYear = document.getElementById('year').value;
 	let movieStatus = document.getElementById('status').value;
+	let movieCover = document.getElementById('cover').value;
 
 	movies.push({
 		title: movieTitle,
 		director: movieDirector,
 		year: movieYear,
 		status: movieStatus,
+		cover: movieCover,
 	});
 
 	displayMovies();
@@ -22,9 +27,20 @@ function addMovie(){
 function displayMovies(){
 	document.getElementById('display').innerHTML = '';
 	for(let i = 0; i < movies.length; i++){
-		document.getElementById('display').innerHTML +=    `<h1>${movies[i].title} directed by ${movies[i].director} </h1>
-			<h1>${movies[i].year} <h1>Shown Status${movies[i].status} />`;
+		document.getElementById('display').innerHTML += `<h1>${movies[i].title} directed by ${movies[i].director} </h1>
+			<h1>${movies[i].year} Shown Status: ${movies[i].status} </h1> <img src=${movies[i].cover} />
+			<input type="button" value="DELETE" onclick="deleteMovies(${i})"/>
+		<hr/>`;
 	}
-
-	
+	//Section to display quantity from id on index
+	document.getElementById('quantityDisplay').innerHTML = 'Quantity: ' + movies.length;
 }
+
+
+function deleteMovies(index){  //has parameter, index b/c it needs a position
+	//1st argument is the index
+	//2nd argument number of element remove
+	movies.splice(index,1); //Given an item, take 1 element out
+	displayMovies();
+}
+
